9ecd7a0798bb35c6c09546851c89939e6d866c40
abunti733 <128597637+abunti733@users.noreply.github.com>
2023-05-21 21:18:42 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,21 @@
+// // function 1st method
+// function additionTwoNumber(x,y,z){
+	// var total=x+y+z;
+	// return total;
+// }
+// console .log(additionTwoNumber(50,50,50))
+
+// 2nd method 
+
+// function additionTwoNumber(x,y,z){
+	// var total=x+y+z;
+	// console.log(total)
+// }
+// additionTwoNumber(30,20,50);
+
+function myFunction(){
+	var num1=number (document.getElementById('num1'));
+	var num1=number (document.getElementById('num2'));
+	var total =num1.value+num2.value;
+	document.write(total);
+}
